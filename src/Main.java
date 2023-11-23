4c13de34767730d1c5d87b84743aa9f89b3c3911
syllaT <130776784+TallysNogueira@users.noreply.github.com>
2023-11-23 14:26:34 -0300

    adição do arquivo main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.awt.EventQueue;
+
+import login_cadastro.SignIn;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SignIn frame = new SignIn();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+}
